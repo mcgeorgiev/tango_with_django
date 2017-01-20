7ba1bf7351f5c830dd566320c426190df395037a
Michael-Windows <michael_georgiev@hotmail.co.uk>
2017-01-20 13:03:24 +0000

    added basic index template

--- twd/rango/views.py
+++ twd/rango/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Hello World!</h1><br/>Please click <a href='/rango/about'>here</a> to view the about page")
+    # Constructs a dict to pass to the template as its context
+    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # Returns a rendered response to send to the client
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     return HttpResponse("<h1>About</h1><br/>This is the about page.</br><a href='/rango/'>Back</a>")
